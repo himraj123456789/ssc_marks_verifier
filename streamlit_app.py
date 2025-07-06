22be5966f4a63e16a275a6b6df0e8b6e128e275c
himraj123456789 <39591573+himraj123456789@users.noreply.github.com>
2025-07-06 18:33:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def process_url(url):
     r = requests.get(url)
      
     soup = BeautifulSoup(r.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
-
+    st.write(soup)
 
 
     # Find all <td> elements with class "bold"
